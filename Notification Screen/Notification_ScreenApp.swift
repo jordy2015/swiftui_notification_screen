0d04dccc7c39ae348c6dcf12cb17a2bd14d96d19
jordy2015 <jordy.g2012@gmail.com>
2023-02-15 10:27:17 -0600

    Initial Commit

---
+++ Notification Screen/Notification_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notification_ScreenApp.swift
+//  Notification Screen
+//
+//  Created by Jordy Gonzalez on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Notification_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
